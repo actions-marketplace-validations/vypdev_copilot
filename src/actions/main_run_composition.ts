8ee4ae951cf335f5e750cea19ff01c2d20b03244
Efra Espada <efraespada@gmail.com>
2026-08-18 09:49:50 +0000

    master: isolate main run composition

---
+++ src/actions/main_run_composition.ts
@@ -0,0 +1,61 @@
+import { DetectPotentialProblemsUseCase } from '../application/usecases/steps/commit/detect_potential_problems_use_case';
+import { DetectBugbotFixIntentUseCase } from '../application/usecases/steps/commit/bugbot/detect_bugbot_fix_intent_use_case';
+import { SingleActionUseCase } from '../application/usecases/single_action_use_case';
+import { DeployedActionUseCase } from '../application/usecases/actions/deployed_action_use_case';
+import { PublishGithubActionUseCase } from '../application/usecases/actions/publish_github_action_use_case';
+import { CreateReleaseUseCase } from '../application/usecases/actions/create_release_use_case';
+import { CreateTagUseCase } from '../application/usecases/actions/create_tag_use_case';
+import { ThinkUseCase } from '../application/usecases/steps/common/think_use_case';
+import { RecommendStepsUseCase } from '../application/usecases/actions/recommend_steps_use_case';
+import { DefaultAgentRepositoryFactory } from '../data/repository/agent_repository_factory';
+import { RepositoryFactory } from '../infrastructure/composition/repository_factory';
+import type { BugbotContextPorts, BugbotWritePorts } from '../application/ports/bugbot_ports';
+
+export function createDetectPotentialProblemsUseCase(factory: RepositoryFactory): DetectPotentialProblemsUseCase {
+    const issueRepository = factory.createBugbotIssueRepository();
+    const pullRequestRepository = factory.createBugbotPullRequestRepository();
+    const contextPorts: BugbotContextPorts = { issue: issueRepository, pullRequest: pullRequestRepository };
+    const writePorts: BugbotWritePorts = { issueComments: issueRepository, pullRequestComments: pullRequestRepository };
+    return new DetectPotentialProblemsUseCase(
+        new DefaultAgentRepositoryFactory().createFindings(),
+        contextPorts,
+        writePorts,
+    );
+}
+
+export function createBugbotContextPorts(factory: RepositoryFactory) {
+    return {
+        issue: factory.createBugbotIssueRepository(),
+        pullRequest: factory.createBugbotPullRequestRepository(),
+    };
+}
+
+export function createDetectBugbotFixIntentUseCase(factory: RepositoryFactory): DetectBugbotFixIntentUseCase {
+    const contextPorts = createBugbotContextPorts(factory);
+    return new DetectBugbotFixIntentUseCase(
+        contextPorts.pullRequest,
+        new DefaultAgentRepositoryFactory().createFindings(),
+        contextPorts,
+    );
+}
+
+export function createSingleActionUseCase(factory: RepositoryFactory): SingleActionUseCase {
+    const repositoryReleasePort = factory.createRepositoryReleaseRepository();
+    const issueDescriptionQueryPort = factory.createIssueContentRepository();
+    return new SingleActionUseCase(
+        new DeployedActionUseCase(
+            factory.createIssueLabelRepository(),
+            factory.createIssueClosureRepository(),
+            factory.createMergeRepository(),
+        ),
+        new PublishGithubActionUseCase(repositoryReleasePort),
+        new CreateReleaseUseCase(repositoryReleasePort),
+        new CreateTagUseCase(repositoryReleasePort),
+        new ThinkUseCase(issueDescriptionQueryPort, factory.createIssueNotificationRepository(), new DefaultAgentRepositoryFactory().createFindings()),
+        factory.createInitialSetupUseCase(),
+        factory.createCheckProgressUseCase(),
+        createDetectPotentialProblemsUseCase(factory),
+        new RecommendStepsUseCase(issueDescriptionQueryPort, new DefaultAgentRepositoryFactory().createFindings()),
+    );
+}
+
--- src/actions/main_run_dispatcher.ts
+++ src/actions/main_run_dispatcher.ts
@@ -4,27 +4,19 @@ import { Result } from '../data/model/result';
 import { CommitUseCase } from '../application/usecases/commit_use_case';
 import { NotifyNewCommitOnIssueUseCase } from '../application/usecases/steps/commit/notify_new_commit_on_issue_use_case';
 import { CheckChangesIssueSizeUseCase } from '../application/usecases/steps/commit/check_changes_issue_size_use_case';
-import { DetectPotentialProblemsUseCase } from '../application/usecases/steps/commit/detect_potential_problems_use_case';
 import { BugbotAutofixUseCase } from '../application/usecases/steps/commit/bugbot/bugbot_autofix_use_case';
-import { DetectBugbotFixIntentUseCase } from '../application/usecases/steps/commit/bugbot/detect_bugbot_fix_intent_use_case';
 import { DoUserRequestUseCase } from '../application/usecases/steps/commit/user_request_use_case';
 import { ProjectBoardCommandPort } from '../application/ports/project_board_ports';
 import { RepositoryFactory } from '../infrastructure/composition/repository_factory';
 import { GitCommitAdapter } from '../infrastructure/git_commit_adapter';
 import type { MainRunRoute } from './main_run_route';
+import { createDetectPotentialProblemsUseCase, createBugbotContextPorts, createDetectBugbotFixIntentUseCase, createSingleActionUseCase } from './main_run_composition';
 import { IssueCommentUseCase } from '../application/usecases/issue_comment_use_case';
 import { CheckIssueCommentLanguageUseCase } from '../application/usecases/steps/issue_comment/check_issue_comment_language_use_case';
 import { PullRequestReviewCommentUseCase } from '../application/usecases/pull_request_review_comment_use_case';
 import { CheckPullRequestCommentLanguageUseCase } from '../application/usecases/steps/pull_request_review_comment/check_pull_request_comment_language_use_case';
-import { SingleActionUseCase } from '../application/usecases/single_action_use_case';
-import { DeployedActionUseCase } from '../application/usecases/actions/deployed_action_use_case';
-import { PublishGithubActionUseCase } from '../application/usecases/actions/publish_github_action_use_case';
-import { CreateReleaseUseCase } from '../application/usecases/actions/create_release_use_case';
-import { CreateTagUseCase } from '../application/usecases/actions/create_tag_use_case';
 import { ThinkUseCase } from '../application/usecases/steps/common/think_use_case';
-import { RecommendStepsUseCase } from '../application/usecases/actions/recommend_steps_use_case';
 import { DefaultAgentRepositoryFactory } from '../data/repository/agent_repository_factory';
-import type { BugbotContextPorts, BugbotWritePorts } from '../application/ports/bugbot_ports';
 import { logDebugInfo, logError, logInfo } from '../utils/logger';
 
 export async function dispatchMainRunRoute(
@@ -34,54 +26,6 @@ export async function dispatchMainRunRoute(
     factory: RepositoryFactory,
 ): Promise<Result[]> {
     const results: Result[] = [];
-
-    function createDetectPotentialProblemsUseCase(factory: RepositoryFactory): DetectPotentialProblemsUseCase {
-        const issueRepository = factory.createBugbotIssueRepository();
-        const pullRequestRepository = factory.createBugbotPullRequestRepository();
-        const contextPorts: BugbotContextPorts = { issue: issueRepository, pullRequest: pullRequestRepository };
-        const writePorts: BugbotWritePorts = { issueComments: issueRepository, pullRequestComments: pullRequestRepository };
-        return new DetectPotentialProblemsUseCase(
-            new DefaultAgentRepositoryFactory().createFindings(),
-            contextPorts,
-            writePorts,
-        );
-    }
-
-    function createBugbotContextPorts(factory: RepositoryFactory) {
-        return {
-            issue: factory.createBugbotIssueRepository(),
-            pullRequest: factory.createBugbotPullRequestRepository(),
-        };
-    }
-
-    function createDetectBugbotFixIntentUseCase(factory: RepositoryFactory): DetectBugbotFixIntentUseCase {
-        const contextPorts = createBugbotContextPorts(factory);
-        return new DetectBugbotFixIntentUseCase(
-            contextPorts.pullRequest,
-            new DefaultAgentRepositoryFactory().createFindings(),
-            contextPorts,
-        );
-    }
-
-    function createSingleActionUseCase(factory: RepositoryFactory): SingleActionUseCase {
-        const repositoryReleasePort = factory.createRepositoryReleaseRepository();
-        const issueDescriptionQueryPort = factory.createIssueContentRepository();
-        return new SingleActionUseCase(
-            new DeployedActionUseCase(
-                factory.createIssueLabelRepository(),
-                factory.createIssueClosureRepository(),
-                factory.createMergeRepository(),
-            ),
-            new PublishGithubActionUseCase(repositoryReleasePort),
-            new CreateReleaseUseCase(repositoryReleasePort),
-            new CreateTagUseCase(repositoryReleasePort),
-            new ThinkUseCase(issueDescriptionQueryPort, factory.createIssueNotificationRepository(), new DefaultAgentRepositoryFactory().createFindings()),
-            factory.createInitialSetupUseCase(),
-            factory.createCheckProgressUseCase(),
-            createDetectPotentialProblemsUseCase(factory),
-            new RecommendStepsUseCase(issueDescriptionQueryPort, new DefaultAgentRepositoryFactory().createFindings()),
-        );
-    }
 
             switch (route) {
                 case 'single-action': {
