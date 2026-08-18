3721afcd1206d18dcd9a03830117fd30eb4b8eb2
Efra Espada <efraespada@gmail.com>
2026-08-18 17:19:06 +0000

    master: isolate cli bootstrap and command registry

--- src/cli.ts
+++ src/cli.ts
@@ -1,23 +1,8 @@
 #!/usr/bin/env node
 
-import * as dotenv from 'dotenv';
-import { Command } from 'commander';
-import { registerThinkCommand } from './cli/commands/think';
-import { registerDoCommand } from './cli/commands/do';
-import { registerCheckProgressCommand } from './cli/commands/check_progress';
-import { registerRecommendStepsCommand } from './cli/commands/recommend_steps';
-import { registerDetectPotentialProblemsCommand } from './cli/commands/detect_potential_problems';
-import { registerSetupCommand } from './cli/commands/setup';
+import { createCliProgram } from './cli/cli_program';
 
-dotenv.config();
-
-const program = new Command();
-registerThinkCommand(program);
-registerDoCommand(program);
-registerCheckProgressCommand(program);
-registerRecommendStepsCommand(program);
-registerDetectPotentialProblemsCommand(program);
-registerSetupCommand(program);
+const program = createCliProgram();
 
 if (typeof process.env.JEST_WORKER_ID === 'undefined') {
   program.parse(process.argv);
